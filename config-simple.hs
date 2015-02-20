d2ce17e8f9948b9b39dd4a137748ef22f8b1b227
Getty Ritter <gdritter@galois.com>
2015-02-20 15:28:39 -0800

    propellor spin

--- config-simple.hs
+++ config-simple.hs
@@ -1,49 +1,50 @@
--- This is the main configuration file for Propellor, and is used to build
--- the propellor program.
-
 import Propellor
 import Propellor.CmdLine
-import Propellor.Property.Scheduled
-import qualified Propellor.Property.File as File
-import qualified Propellor.Property.Apt as Apt
-import qualified Propellor.Property.Network as Network
---import qualified Propellor.Property.Ssh as Ssh
-import qualified Propellor.Property.Cron as Cron
---import qualified Propellor.Property.Sudo as Sudo
-import qualified Propellor.Property.User as User
---import qualified Propellor.Property.Hostname as Hostname
---import qualified Propellor.Property.Tor as Tor
-import qualified Propellor.Property.Docker as Docker
+import qualified Propellor.Property.Apt     as Apt
+import qualified Propellor.Property.Cmd     as Cmd
+import qualified Propellor.Property.Git     as Git
 
 main :: IO ()
-main = defaultMain hosts
+main = do
+  print buildHost
+  defaultMain [host "lemuria", buildHost]
 
--- The hosts propellor knows about.
--- Edit this to configure propellor!
-hosts :: [Host]
-hosts =
-	[ host "mybox.example.com"
-		& os (System (Debian Unstable) "amd64")
-		& Apt.stdSourcesList
-		& Apt.unattendedUpgrades
-		& Apt.installed ["etckeeper"]
-		& Apt.installed ["ssh"]
-		& User.hasSomePassword "root"
-		& Network.ipv6to4
-		& File.dirExists "/var/www"
-		& Docker.docked webserverContainer
-		& Docker.garbageCollected `period` Daily
-		& Cron.runPropellor (Cron.Times "30 * * * *")
+cabalInstall :: String -> Property NoInfo
+cabalInstall pkgs = Cmd.cmdProperty "cabal" ["install", pkgs]
 
-	-- add more hosts here...
-	--, host "foo.example.com" = ...
-	]
+allOf :: [Property NoInfo] -> Property NoInfo
+allOf xs = simpleProperty "Finished list" (return NoChange) xs
 
--- A generic webserver in a Docker container.
-webserverContainer :: Docker.Container
-webserverContainer = Docker.container "webserver" "joeyh/debian-stable"
-	& os (System (Debian (Stable "wheezy")) "amd64")
-	& Apt.stdSourcesList
-	& Docker.publish "80:80"
-	& Docker.volume "/var/www:/var/www"
-	& Apt.serviceInstalledRunning "apache2"
+buildHost :: Host
+buildHost =
+  let usr = "build"
+      dir = "/build"
+      ivoryRepo = dir <> "/ivory"
+      towerRepo = dir <> "/tower"
+      bspRepo   = dir <> "/ivory-tower-stm32"
+  in host "sbc"
+     & os (System (Debian (Stable "jessie")) "amd64")
+     & Apt.stdSourcesList
+     & Apt.update
+     & Apt.installed ["ghc"]
+     & Apt.setSourcesList ["ppa:terry.guo/gcc-arm-embedded"]
+     & Apt.installed ["gcc-arm-none-eabi"]
+     & Git.cloned usr "https://github.com/galoisinc/ivory" ivoryRepo Nothing
+     & Git.cloned usr "https://github.com/galoisinc/tower" towerRepo Nothing
+     & Git.cloned usr "https://github.com/galoisinc/ivory-tower-stm32" bspRepo Nothing
+     & cabalInstall "cabal-install"
+     & cabalInstall "alex"
+     & cabalInstall "happy"
+     & allOf [ Cmd.cmdProperty' "make" [target]
+               [ ("IVORY_REPO", ivoryRepo)
+               , ("TOWER_REPO", towerRepo)
+               , ("BSP_REPO",   bspRepo)
+               ]
+             | target <- [ "ivory-px4-hw"
+                         , "ivory-px4-tests"
+                         , "smaccm-commsec"
+                         , "smaccm-mavlink"
+                         , "smaccm-datalink"
+                         , "smaccm-ins"
+                         ]
+             ]
